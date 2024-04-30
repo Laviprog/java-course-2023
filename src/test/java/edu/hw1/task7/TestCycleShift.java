4b13f230e64527ab67e2bb0d593870f50f7f2e46
laviprog <areyouready@internet.ru>
2024-04-30 12:22:07 +0300

    project1

--- src/test/java/edu/hw1/task7/TestCycleShift.java
+++ src/test/java/edu/hw1/task7/TestCycleShift.java
@@ -3,6 +3,7 @@ package edu.hw1.task7;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestCycleShift {
